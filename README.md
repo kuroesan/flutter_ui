9390acdfb87efb1c20b85215994b5994ec23bf80
xuguanhua <761623506@qq.com>
2019-04-28 17:24:26 +0800

    add avatar page

--- README.md
+++ README.md
@@ -15,5 +15,11 @@
 <img src="./screenshots/screenshots_4.png" width="240" />
 <img src="./screenshots/screenshots_5.png" width="240" />
 <img src="./screenshots/screenshots_6.png" width="240" />
+
+## 视图
+
+<div style="display:flex">
+<img src="./screenshots/screenshots_7.png" width="240" />
+
 </div>
 
--- lib/routes/view/avatar.dart
+++ lib/routes/view/avatar.dart
@@ -20,10 +20,91 @@ class AvatarPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               TitleWidget(title: title),
+              SubTitle(title: "圆形头像"),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0),
+                      child: FUAvatar(
+                        size: "small",
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0),
+                      child: FUAvatar(),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0),
+                      child: FUAvatar(
+                        size: "large",
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SubTitle(title: "圆角矩形头像"),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0),
+                      child: FUAvatar(size: "small", type: "rect"),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0),
+                      child: FUAvatar(type: "rect"),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 30.0),
+                      child: FUAvatar(size: "large", type: "rect"),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class FUAvatar extends StatelessWidget {
+  FUAvatar(
+      {this.size = "default",
+      this.type = "circle",
+      this.image = "assets/images/flutter_logo_bg.png"});
+  final String size;
+  final String type;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    var avatarWidth = 72.0;
+    if (this.size == "small") {
+      avatarWidth = 58.0;
+    } else if (this.size == "large") {
+      avatarWidth = 84.0;
+    } else {
+      avatarWidth = 72.0;
+    }
+
+    return this.type == "circle"
+        ? ClipOval(
+            child: Container(
+              child: Image.asset(
+                this.image,
+                width: avatarWidth,
+              ),
+            ),
+          )
+        : ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            child: Image.asset(
+              this.image,
+              width: avatarWidth,
+            ),
+          );
   }
 }
